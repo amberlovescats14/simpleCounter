142d3cef4ce3987670200a250c71f1adcb3d666b
Amber Jones <amberlovescats14@gmail.com>
2019-05-22 20:20:48 -0500

    counter

---
+++ counter/src/components/Counter.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { connect } from 'react-redux'
+
+function Counter(props) {
+  return (
+    <div>
+      <h1>This is a Counter</h1>
+      <h1>{props.count}</h1>
+      <button onClick={props.onIncrement}>Incrament</button>
+      <button onClick={props.onDecrement}>Decrament</button>
+    </div>
+  )
+}
+const mapStateToProps = state =>({
+
+   count: state.count
+} )
+const mapDispatchToProps = dispatch => ({
+  onIncrement: () => {
+    const action = {type: 'INCREASE_COUNT'};
+    dispatch(action)
+  },
+  onDecrement: () => {
+    const action = {type: 'DECREASE_COUNT'};
+    dispatch(action)
+  }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter)
